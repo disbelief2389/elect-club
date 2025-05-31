906db08ef62a01c831ebab639d3aab75a9373eaa
disbelief2389 <v3i1ix@proton.me>
2025-05-31 20:32:56 +1000

    Better setup than 1.0

---
+++ js/load-common.js
@@ -0,0 +1,21 @@
+function loadHTML(elementId, filePath) {
+	const element = document.getElementById(elementId);
+	if (!element) {
+		console.error(`Element with id ${elementId} not found`);
+		return;
+	}
+
+	fetch(filePath)
+		.then((response) => {
+			if (!response.ok)
+				throw new Error("Network error: " + response.statusText);
+			return response.text();
+		})
+		.then((data) => (element.innerHTML = data))
+		.catch((error) => console.error("Error loading HTML:", error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	loadHTML("navbar", "navbar.html");
+	loadHTML("footer", "footer.html");
+});
